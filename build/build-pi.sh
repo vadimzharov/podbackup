7b65469f34fe0c930abc76e3e3da61d7b630a924
Vadim Zharov <vzharov81@gmail.com>
2023-01-13 22:23:58 -0600

    build-pi deleted

--- build/build-pi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ARM_VERSION=7
-echo "Building ARM image"
-env GOOS=linux GOARCH=arm GOARM=$ARM_VERSION go build -o podbackup
-podman build -t quay.io/vadimzharov/podbackup:latest .
-podman push quay.io/vadimzharov/podbackup:latest
-rm podbackup
